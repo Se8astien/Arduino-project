a8a15b92f6ea80810520ae5011b68f3ffc1f2564
Sébastien Mansion <sebastienmansion@mbpdesebastien.home>
2016-06-07 22:27:02 +0200

    add servo control

--- test.js
+++ test.js
@@ -15,43 +15,74 @@ app.get('/', function(req, res) {
 
 httpServer.listen(port);
 console.log('Server available at http://localhost:' + port);
-var led;
 
+var led;
+var servo;
+var tempSensor;
+var oldtemp;
+var socket;
 //Arduino board connection
 
 var board = new five.Board();
-board.on("ready", function() {
-    console.log('Arduino connected');
-    led = new five.Led(13);
-
-});
+var connected=false;
 
 //Socket connection handler
-io.on('connection', function (socket) {
+io.on('connection', function (s) {
+        socket = s;
+        connected = true;
+
         console.log('socket id : '+socket.id);
 
-        var temperature = new five.Thermometer({
-          controller: "TMP36",
-          pin: "A0"
-        });
-
-        temperature.on("change", function() {
-          console.log(this.celsius + "°C", this.fahrenheit + "°F");
-          socket.emit('temperature', { some: this.celsius });
-        });
         socket.on('led:on', function (data) {
            led.on();
-           console.log('LED ON RECEIVED'+temperature.celsius);
+           console.log('LED ON RECEIVED');
         });
 
         socket.on('led:off', function (data) {
             led.off();
             console.log('LED OFF RECEIVED');
+        });
 
+        // control servo
+        socket.on('servo:max90', function (data) {
+            console.log('servo add 20');
+            servo.to(90, 2000, 10);
+            //servo.to(20);
+        });
+        socket.on('servo:min90', function (data) {
+            console.log('servo go to -90');
+          servo.to(-90, 2000, 10);
+            //servo.to(-20);
+        });
+        socket.on('servo:center', function (data) {
+            console.log('servo center');
+        servo.to(45, 2000, 10);
+            //servo.to(-20);
         });
 
 });
 
+board.on("ready", function() {
+    console.log('Arduino connected');
+    led        = new five.Led(13);
+    servo      = new five.Servo({
+      startAt: 45,
+      pin :10
+    });
+    tempSensor = new five.Thermometer({
+      controller: "TMP36",
+      pin: "A0"
+    });
+
+    tempSensor.on('data', function() {
+      // if temp change then send data
+      if(this.celsius != oldtemp){
+        console.log(this.celsius + "°C", this.fahrenheit + "°F");
+        if(connected) socket.emit('tempSensor', { some: this.celsius });
+      }
+      oldtemp = this.celsius;
+    });
+});
 
 
 
